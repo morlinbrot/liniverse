98d3f019b25963ef379b8e732c2a8d26a4703d8a
morlinbrot <morlinbrot@mailbox.org>
2019-11-06 20:03:08 +0100

    Update Rect, add doc comments

--- src/rect.rs
+++ src/rect.rs
@@ -15,6 +15,8 @@ pub struct Rect {
     height: f64,
 }
 
+/// A struct representing a rectangular plane in a cartesian coordinate system.
+/// X and y co-ordinates specify the *center* of the rectangle.
 impl Rect {
     pub fn new(x: f64, y: f64, width: f64, height: f64) -> Self {
         Self {
@@ -26,25 +28,50 @@ impl Rect {
     }
 
     pub fn contains(&self, p: &Point) -> bool {
-        let top_left = Point::new(self.x - self.width / 2.0, self.y - self.height / 2.0);
-        let bot_right = Point::new(self.x + self.width / 2.0, self.y + self.height / 2.0);
+        let nw = self.corner(Cardinal::NW);
+        let se = self.corner(Cardinal::SE);
 
-        p.x > top_left.x && p.x < bot_right.x && p.y > top_left.y && p.y < bot_right.y
+        p.x > nw.x && p.x < se.x && p.y > nw.y && p.y < se.y
     }
 
+    /// Return a Point at the center of the rectangle.
     pub fn center(&self) -> Point {
         Point::new(self.x, self.y)
     }
 
+    /// Return a Point representing the specified corner of the rectangle.
     pub fn corner(&self, corner: Cardinal) -> Point {
-        let half_width = self.width / 2.0;
-        let half_height = self.height / 2.0;
+        let half_w = self.half_width();
+        let half_h = self.half_height();
         match corner {
-            Cardinal::NW => Point::new(self.x - half_width, self.y + half_height),
-            Cardinal::NE => Point::new(self.x + half_width, self.y + half_height),
-            Cardinal::SE => Point::new(self.x + half_width, self.y - half_height),
-            Cardinal::SW => Point::new(self.x - half_width, self.y - half_height),
+            Cardinal::NW => Point::new(self.x - half_w, self.y + half_h),
+            Cardinal::NE => Point::new(self.x + half_w, self.y + half_h),
+            Cardinal::SE => Point::new(self.x + half_w, self.y - half_h),
+            Cardinal::SW => Point::new(self.x - half_w, self.y - half_h),
         }
+    }
+
+    /// Return a Rect covering the area from the center to the specified corner.
+    pub fn split_rect(&self, c: Cardinal) -> Self {
+        let x = self.center().x;
+        let y = self.center().y;
+        let half_w = self.half_width();
+        let half_h = self.half_height();
+
+        match c {
+            Cardinal::NW => Self::new(x - half_w, y + half_h, half_w, half_h),
+            Cardinal::NE => Self::new(x + half_w, y + half_h, half_w, half_h),
+            Cardinal::SE => Self::new(x + half_w, y - half_h, half_w, half_h),
+            Cardinal::SW => Self::new(x - half_w, y - half_h, half_w, half_h),
+        }
+    }
+
+    fn half_width(&self) -> f64 {
+        self.width / 2.0
+    }
+
+    fn half_height(&self) -> f64 {
+        self.height / 2.0
     }
 }
 
